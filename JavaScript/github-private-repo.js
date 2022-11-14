eeb4ead594637dec2a6c598a401ae3360d5c1c0a
dcpengx <9205548+dcpengx@users.noreply.github.com>
2022-11-14 20:48:11 +0800

    Update github-private-repo.js

--- JavaScript/github-private-repo.js
+++ JavaScript/github-private-repo.js
@@ -1,16 +1,18 @@
 /**
-允许引用Github私有仓库/gist中的订阅分流，重写，脚本等配置文件。❗️仅Loon & Surge可用
-@author: Peng-YM
+允许引用Github私有仓库/gist中的订阅分流，重写，脚本等配置文件。QX专用版
+修改自：@author: Peng-YM (原版支持surge和loon)
+
+
 🛎[配置说明]
 0️⃣ 
-1. 使用Surge模块:
+1. 使用QX重写:
 https://raw.githubusercontent.com/Peng-YM/QuanX/master/Rewrites/GithubPrivate/github-private-repo.sgmodule
-2. 使用Loon插件:
-https://raw.githubusercontent.com/Peng-YM/QuanX/master/Rewrites/GithubPrivate/github-private-repo.plugin
 1️⃣ 登陆Github > 点击头像下拉菜单 > 选择Settings > 左边菜单栏选择最后一个Developer settings > 选择Personal access tokens > Generate new token > Note里面填写token名字 > ☑️下面的勾选框选择第一项repo打钩（所有子项目自动勾选）> 点击Generate token按钮保存并返回。
+（如已有repo权限的token，可直接使用已有的token)
 2️⃣ 在BoxJS里面填入用户名（打开Github，浏览器地址栏应该会显示https://github.com/这里是你的用户名/）和上面的token。
-🔘[QX无法使用的说明]
-QX下载配置文件的请求不会触发脚本MITM重写。故无法使用。
+可直接使用Peng-YM大佬的boxjs订阅：https://raw.githubusercontent.com/Peng-YM/QuanX/master/Tasks/box.js.json)
+
+
 */
 
 // 如果不使用BoxJS配置，可以在这里修改
